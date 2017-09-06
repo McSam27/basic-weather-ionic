84f089a13bedf8ce419ab155f6a89f91520758fa
Sam McCagg <Sam@Owners-MacBook-Pro.local>
2017-09-05 22:30:38 -0400

    finished app

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
 @IonicPage()
 @Component({
   selector: 'page-settings',
@@ -11,14 +12,35 @@ export class SettingsPage {
   city: string;
   state: string;
 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage
+  ) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.city = 'Spencerport';
-    this.state = 'NY';
+    this.storage.get('location').then((val) => {
+      if (val !== null) {
+        let location = JSON.parse(val);
+        this.city = location.city;
+        this.state = location.state;
+      } else {
+        this.city = "Spencerport";
+        this.state = "NY"
+      }
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  saveForm() {
+    let location = {
+      city: this.city,
+      state: this.state
+    };
+    this.storage.set('location', JSON.stringify(location));
+    this.navCtrl.push(HomePage);
   }
 
 }
